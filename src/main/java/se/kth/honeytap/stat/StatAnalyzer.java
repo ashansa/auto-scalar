5aaa1589e2a583dfa3bf94ab948c07ce2c09eba1
ashansa <ashansa.perera@gmail.com>
2016-06-19 00:43:36 +0200

    adding stat analyzer to create tuples

--- src/main/java/se/kth/honeytap/stat/StatAnalyzer.java
+++ src/main/java/se/kth/honeytap/stat/StatAnalyzer.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.TreeMap;
 
 /**
@@ -22,7 +24,13 @@ import java.util.TreeMap;
  */
 public class StatAnalyzer {
   static Log log = LogFactory.getLog(StatAnalyzer.class);
-  static String resultPath = "17June/wl1/10sec_window_2/";
+  static String resultPath = "17June/wl2/10sec_window_4_noBilling/";
+  static String wl1 = "0.5:1, 5:2, 5:3, 5:4, 8:5, 1:4, 1:3, 1:2, 0.5:1";
+  static String wl2 = "1:1, 1:2, 1:3, 1:2, 1:3, 1:3, 1:2, 1:3, 1:2, 1:2, 1:3, 1:3, 1:2, 1:2, 1:1, 1:2, 1:2, 1:3, 1:2, 1:1, 1:3, 1:3, 1:2, 1:3, 1:2";
+
+  public static void main(String[] args) throws Exception {
+    writeFinalResults();
+  }
 
   public static void analyze() {
     try {
@@ -113,10 +121,161 @@ public class StatAnalyzer {
     } catch (IOException e) {
       throw new IllegalStateException(e);
     }
+  }
+
+
+  public static void writeFinalResults() throws Exception {
+
+    BufferedReader br;
+    PrintWriter writer;
+    Queue<Integer> originalReqQueue = getOriginalReq();
+    try {
+      String line = null;
+      br = new BufferedReader(new FileReader("tupleValues.txt"));
+      writer = new PrintWriter(resultPath.concat("finalResult.csv"), "UTF-8");
+      ResultTuple previousTuple = new ResultTuple("6/17/2016 16:06:14", 1, 1, 1, 55.0f);  //time,withFeedbackReq,alloc,originalReq
+      float previousUtilization = 55.0f;
+
+      while ((line = br.readLine()) != null) {
+        String[] elements = line.split(",");
+        String timeString = elements[0].trim();
+        String utilizationString = elements[1].trim();
+        String allocationString = elements[2].trim();
+
+        String time;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date resultDate = new Date(Long.valueOf(timeString));
+        time = sdf.format(resultDate);
+
+        if (time.equals(previousTuple.time)) {
+          continue;      //TODO may be the previous one should be replaced with new one
+        }
+
+        int withFeedbackReq;
+        int alloc = Integer.valueOf(allocationString);
+        int originalReq = originalReqQueue.poll();
+
+       /* float utilization = Float.valueOf(utilizationString);
+        utilization = Float.parseFloat(String.format("%.2f", utilization));
+        if ( (utilization >= 30 && utilization <= 80) || (previousTuple.utilization == utilization && previousTuple.originalReq == originalReq) ) {
+          withFeedbackReq = previousTuple.withFeedbackReq;
+        } else if (utilization > 80) {
+          withFeedbackReq = (int) Math.ceil((utilization/80) * alloc);
+        } else {
+          if (previousUtilization < 30) { // we have already reduced the machine
+            withFeedbackReq = Integer.valueOf(previousTuple.withFeedbackReq);
+          } else {
+            withFeedbackReq = Integer.valueOf(previousTuple.withFeedbackReq) -1;
+          }
+        }*/
+
+       /* float utilization = Float.valueOf(utilizationString);
+        utilization = Float.parseFloat(String.format("%.2f", utilization));   //limiting to two decimal points
+        if (previousTuple.utilization == utilization && previousTuple.originalReq == originalReq) {
+          withFeedbackReq = previousTuple.withFeedbackReq;
+        } else if (utilization >= 30) {
+          withFeedbackReq = (int) Math.ceil((utilization/80) * alloc);
+        } else {
+          if (previousUtilization < 30) { // we have already reduced the machine
+            withFeedbackReq = Integer.valueOf(previousTuple.withFeedbackReq);
+          } else {
+            withFeedbackReq = Integer.valueOf(previousTuple.withFeedbackReq) -1;
+          }
+        }*/
+
+        float utilization = Float.valueOf(utilizationString);
+        utilization = Float.parseFloat(String.format("%.2f", utilization));   //limiting to two decimal points
+        if (previousTuple.utilization == utilization && previousTuple.originalReq == originalReq) {
+          withFeedbackReq = previousTuple.withFeedbackReq;
+        } else if (utilization >= 30 && utilization <= 80) {
+          if (originalReq == alloc) {
+            withFeedbackReq = alloc;
+          } else {
+            withFeedbackReq = (int) Math.ceil((utilization/80) * alloc);
+          }
+        } else if (utilization > 80) {
+          withFeedbackReq = (int) Math.ceil((utilization/80) * alloc);
+        } else {
+          if (previousUtilization < 30) { // we have already reduced the machine
+            withFeedbackReq = Integer.valueOf(previousTuple.withFeedbackReq);
+          } else {
+            withFeedbackReq = Integer.valueOf(previousTuple.withFeedbackReq) -1;
+          }
+        }
+
+        /////////TODO set original req here
+        ResultTuple newTuple = new ResultTuple(time, withFeedbackReq, alloc, originalReq, utilization);
+        previousTuple = newTuple;
+        previousUtilization = utilization;
+        writer.println(newTuple.utilization + "," + newTuple.time + "," + newTuple.withFeedbackReq + "," +
+                newTuple.allocation + "," + newTuple.originalReq);
+      }
+      writer.flush();
+      br.close();
+
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
+    }
     log.info("==========================================================");
     log.info("==========================================================");
-    log.info("================== logging data done===================");
+    log.info("================== Stored final output ===================");
     log.info("==========================================================");
     log.info("==========================================================");
+  }
+
+  private static Queue<Integer> getOriginalReq() throws Exception {
+    Queue<Integer> reqQueue = new LinkedList<>();
+
+    //originalReqDef:    0.5:1, 5:2, 5:3, 5:4, 8:5, 1:4, 1:3, 1:2, 0.5:1
+    String originalReqDef = "";
+    if (resultPath.contains("wl1")) {
+      originalReqDef = wl1;
+    } else if (resultPath.contains("wl2")) {
+      originalReqDef = wl2;
+    } else {
+      throw new Exception("Couldnt find the matching workload");
+    }
+    String[] reqArray = originalReqDef.split(",");
+    for (String req : reqArray) {
+      String durationString = req.trim().split(":")[0];
+      int currentReq = Integer.valueOf(req.trim().split(":")[1]);
+      int durationMin = 0;
+      int durationSec = 0;
+      if (durationString.contains(".")) {
+        //will have both min and sec
+        durationMin = Integer.valueOf(durationString.split("\\.")[0]);
+        durationSec = Integer.valueOf(durationString.split("\\.")[1]);
+      } else {
+        durationMin = Integer.valueOf(durationString);
+      }
+
+      if (durationMin > 0) {
+        for (int i = 0; i < durationMin * 60; i ++) {
+          reqQueue.add(currentReq);
+        }
+      }
+      if (durationSec > 0) {
+        for (int i = 0; i < durationSec; i ++) {
+          reqQueue.add(currentReq);
+        }
+      }
+    }
+    return reqQueue;
+  }
+
+  static class ResultTuple {
+    String time;
+    int withFeedbackReq;
+    int allocation;
+    int originalReq;
+    float utilization;
+
+    public ResultTuple(String time, int withFeedbackReq, int allocation, int originalReq, float utilization) {
+      this.time = time;
+      this.withFeedbackReq = withFeedbackReq;
+      this.allocation = allocation;
+      this.originalReq = originalReq;
+      this.utilization = utilization;
+    }
   }
 }
