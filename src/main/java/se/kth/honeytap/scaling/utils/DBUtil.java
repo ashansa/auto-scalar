87903637ae4382ca680a7cb8b522ce15049f568c
Kamal Hakimzadeh <kamal@Mahmouds-iMac.local>
2016-07-22 12:41:59 +0200

    reading db connection file from the classpath

--- src/main/java/se/kth/honeytap/scaling/utils/DBUtil.java
+++ src/main/java/se/kth/honeytap/scaling/utils/DBUtil.java
@@ -19,153 +19,145 @@ import java.util.Properties;
  */
 public class DBUtil {
 
-    private static final Log log = LogFactory.getLog(DBUtil.class);
+  private static final Log log = LogFactory.getLog(DBUtil.class);
+  private static String PROP_FILE_PATH = "dbConnection.properties";
 
-    //TODO copy the properties file to outside and use only the dbConnection.properties as the below value
-    private static final String DB_PROPERTY_FILE = "/karamel-core/src/main/resources/dbConnection.properties";
+  //TODO copy the properties file to outside and use only the dbConnection.properties as the below value
+  public static Connection getDBConnection() throws DBConnectionFailureException {
+    Properties prop = new Properties();
+    InputStream in = null;
 
-    public static Connection getDBConnection() throws DBConnectionFailureException {
-        Properties prop = new Properties();
-        InputStream in = null;
-        String propertyFilePath = DB_PROPERTY_FILE;
-        try {
-            propertyFilePath =  getPropertyFilePath();
-            in = new FileInputStream(new File(propertyFilePath));
-            prop.load(in);
-            in.close();
+    try {
+      in = DBUtil.class.getClassLoader().getResourceAsStream(PROP_FILE_PATH);
+      prop.load(in);
+      in.close();
 
-        } catch (FileNotFoundException e) {
-            DBConnectionFailureException exception = handleDBConnectionException(e.getMessage(), e);
-            throw exception;
-        } catch (IOException e) {
-            DBConnectionFailureException exception = handleDBConnectionException("Error occurred while reading " +
-                    propertyFilePath, e);
-            throw exception;
-        }
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            String urlHostPort = prop.getProperty("db.urlhostport");
-            String dbName = prop.getProperty("db.name");
-            String username = prop.getProperty("db.username");
-            String password = prop.getProperty("db.password");
-            String connectionURL;
-
-            if (urlHostPort.endsWith("/"))
-                connectionURL = urlHostPort.concat(dbName);
-            else
-                connectionURL = urlHostPort.concat("/").concat(dbName);
-
-            Connection connection = DriverManager.getConnection(connectionURL, username, password);
-            log.info("DB connection successful to " + dbName + " with user " + username);
-            return connection;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            DBConnectionFailureException exception = handleDBConnectionException(
-                    "Error occurred while connecting to database.", e);
-            throw exception;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            DBConnectionFailureException exception = handleDBConnectionException("DB driver class not found", e);
-            throw exception;
-        }
+    } catch (FileNotFoundException e) {
+      DBConnectionFailureException exception = handleDBConnectionException(e.getMessage(), e);
+      throw exception;
+    } catch (IOException e) {
+      DBConnectionFailureException exception = handleDBConnectionException("Error occurred while reading "
+          + PROP_FILE_PATH, e);
+      throw exception;
     }
 
-    public static Connection getInMemoryDBConnection() throws DBConnectionFailureException {
-        Properties prop = new Properties();
-        InputStream in = null;
-        String propertyFilePath = DB_PROPERTY_FILE;
-        try {
-            propertyFilePath =  getPropertyFilePath();
-            in = new FileInputStream(new File(propertyFilePath));
-            prop.load(in);
-            in.close();
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
 
-        } catch (FileNotFoundException e) {
-            DBConnectionFailureException exception = handleDBConnectionException(e.getMessage(), e);
-            throw exception;
-        } catch (IOException e) {
-            DBConnectionFailureException exception = handleDBConnectionException("Error occurred while reading " +
-                    propertyFilePath, e);
-            throw exception;
-        }
+      String urlHostPort = prop.getProperty("db.urlhostport");
+      String dbName = prop.getProperty("db.name");
+      String username = prop.getProperty("db.username");
+      String password = prop.getProperty("db.password");
+      String connectionURL;
 
-        try {
+      if (urlHostPort.endsWith("/")) {
+        connectionURL = urlHostPort.concat(dbName);
+      } else {
+        connectionURL = urlHostPort.concat("/").concat(dbName);
+      }
 
-            Class.forName("org.hsqldb.jdbcDriver");
-            String dbName = prop.getProperty("db.name");
-            Connection connection =  DriverManager.getConnection("jdbc:hsqldb:mem:" + dbName, "SA", "");  //creates ruleDB if not exists
+      Connection connection = DriverManager.getConnection(connectionURL, username, password);
+      log.info("DB connection successful to " + dbName + " with user " + username);
+      return connection;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      DBConnectionFailureException exception = handleDBConnectionException(
+          "Error occurred while connecting to database.", e);
+      throw exception;
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      DBConnectionFailureException exception = handleDBConnectionException("DB driver class not found", e);
+      throw exception;
+    }
+  }
 
-            log.info("DB connection successful to " + dbName + " with user SA");
-            return connection;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            DBConnectionFailureException exception = handleDBConnectionException(
-                    "Error occurred while connecting to database.", e);
-            throw exception;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            DBConnectionFailureException exception = handleDBConnectionException("DB driver class not found", e);
-            throw exception;
-        }
+  public static Connection getInMemoryDBConnection() throws DBConnectionFailureException {
+    Properties prop = new Properties();
+    InputStream in = null;
+    try {
+      in = DBUtil.class.getClassLoader().getResourceAsStream(PROP_FILE_PATH);
+      prop.load(in);
+      in.close();
+
+    } catch (FileNotFoundException e) {
+      DBConnectionFailureException exception = handleDBConnectionException(e.getMessage(), e);
+      throw exception;
+    } catch (IOException e) {
+      DBConnectionFailureException exception = handleDBConnectionException("Error occurred while reading "
+          + PROP_FILE_PATH, e);
+      throw exception;
     }
 
-    public static Connection getKandyDbConnection() throws DBConnectionFailureException {
-        Properties prop = new Properties();
-        InputStream in = null;
-        String propertyFilePath = DB_PROPERTY_FILE;
-        try {
-            propertyFilePath =  getPropertyFilePath();
-            in = new FileInputStream(new File(propertyFilePath));
-            prop.load(in);
-            in.close();
+    try {
 
-        } catch (FileNotFoundException e) {
-            DBConnectionFailureException exception = handleDBConnectionException(e.getMessage(), e);
-            throw exception;
-        } catch (IOException e) {
-            DBConnectionFailureException exception = handleDBConnectionException("Error occurred while reading " +
-                    propertyFilePath, e);
-            throw exception;
-        }
+      Class.forName("org.hsqldb.jdbcDriver");
+      String dbName = prop.getProperty("db.name");
+      Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:" + dbName, "SA", "");  //creates ruleDB if not exists
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
+      log.info("DB connection successful to " + dbName + " with user SA");
+      return connection;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      DBConnectionFailureException exception = handleDBConnectionException(
+          "Error occurred while connecting to database.", e);
+      throw exception;
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      DBConnectionFailureException exception = handleDBConnectionException("DB driver class not found", e);
+      throw exception;
+    }
+  }
 
-            String urlHostPort = prop.getProperty("kandydb.urlhostport");
-            String dbName = prop.getProperty("kandydb.name");
-            String username = prop.getProperty("kandydb.username");
-            String password = prop.getProperty("kandydb.password");
-            String connectionURL;
+  public static Connection getKandyDbConnection() throws DBConnectionFailureException {
+    Properties prop = new Properties();
+    InputStream in = null;
+    try {
+      in = DBUtil.class.getClassLoader().getResourceAsStream(PROP_FILE_PATH);
+      prop.load(in);
+      in.close();
 
-            if (urlHostPort.endsWith("/"))
-                connectionURL = urlHostPort.concat(dbName);
-            else
-                connectionURL = urlHostPort.concat("/").concat(dbName);
-
-            Connection connection = DriverManager.getConnection(connectionURL, username, password);
-            log.info("DB connection successful to " + dbName + " with user " + username);
-            return connection;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            DBConnectionFailureException exception = handleDBConnectionException(
-                    "Error occurred while connecting to database.", e);
-            throw exception;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            DBConnectionFailureException exception = handleDBConnectionException("DB driver class not found", e);
-            throw exception;
-        }
+    } catch (FileNotFoundException e) {
+      DBConnectionFailureException exception = handleDBConnectionException(e.getMessage(), e);
+      throw exception;
+    } catch (IOException e) {
+      DBConnectionFailureException exception = handleDBConnectionException("Error occurred while reading "
+          + PROP_FILE_PATH, e);
+      throw exception;
     }
 
-    public static DBConnectionFailureException handleDBConnectionException(String msg, Exception e) {
-        log.error(msg);
-        e.printStackTrace();
-        return new DBConnectionFailureException(msg, e.getCause());
-    }
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
 
-    private static String getPropertyFilePath() throws IOException {
-        return new File(".").getCanonicalPath().concat(DB_PROPERTY_FILE);
+      String urlHostPort = prop.getProperty("kandydb.urlhostport");
+      String dbName = prop.getProperty("kandydb.name");
+      String username = prop.getProperty("kandydb.username");
+      String password = prop.getProperty("kandydb.password");
+      String connectionURL;
+
+      if (urlHostPort.endsWith("/")) {
+        connectionURL = urlHostPort.concat(dbName);
+      } else {
+        connectionURL = urlHostPort.concat("/").concat(dbName);
+      }
+
+      Connection connection = DriverManager.getConnection(connectionURL, username, password);
+      log.info("DB connection successful to " + dbName + " with user " + username);
+      return connection;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      DBConnectionFailureException exception = handleDBConnectionException(
+          "Error occurred while connecting to database.", e);
+      throw exception;
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+      DBConnectionFailureException exception = handleDBConnectionException("DB driver class not found", e);
+      throw exception;
     }
+  }
+
+  public static DBConnectionFailureException handleDBConnectionException(String msg, Exception e) {
+    log.error(msg);
+    e.printStackTrace();
+    return new DBConnectionFailureException(msg, e.getCause());
+  }
 }
